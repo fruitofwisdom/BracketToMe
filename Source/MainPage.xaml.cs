2914df5efd8606bb301aa2fe12892e5a1c776daa
Ward Childress <ward.childress@gmail.com>
2022-03-15 16:07:54 -0400

    Added data for the 2022 NCAA Men's Basketball Tournament.
    Added a set of team data for the 2022 NCAA Men's Basketball Tournament. The Open Team Data button will let users pick their data sets which are then parsed into Teams.

---
+++ Source/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BracketToMe
+{
+    /// <summary>
+    /// The main page for the tournament results and interacted with the simulation.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private TeamData teamData = new TeamData();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OpenTeamDataButtonClick(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.List;
+            picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            picker.FileTypeFilter.Add(".csv");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.Add(file);
+                await teamData.ReadFile(file);
+
+                if (teamData.teams.Count > 0)
+                {
+                    Button runSimulationButton = FindName("RunSimulationButton") as Button;
+                    runSimulationButton.IsEnabled = true;
+                }
+            }
+            else
+            {
+                // Do something else
+            }
+        }
+
+        private void RunSimulationButtonClick(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+    }
+}
---
+++ Source/TeamData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketToMe
+{
+    internal class Team
+    {
+        public int Id;
+        public string Name;
+        public int Seed;
+        public int BpiRank;
+        public float BpiOff;
+        public float BpiDef;
+        public int RecordW;
+        public int RecordL;
+        public int ConferenceW;
+        public int ConferenceL;
+        public int VsTop25W;
+        public int VsTop25L;
+        public int Last10W;
+        public int Last10L;
+        public int SosRank;
+        public int SorRank;
+        public float Ppg;
+        public float OppPgg;
+        public float FieldGoalPer;
+        public float ThreePointPer;
+        public float FreeThrowPer;
+
+        public Team(string[] fields)
+        {
+            Id = fields[0] == "" ? 0 : int.Parse(fields[0]);
+            Name = fields[1].Trim();
+            Seed = fields[2] == "" ? 0 : int.Parse(fields[2]);
+            BpiRank = fields[3] == "" ? 0 : int.Parse(fields[3]);
+            BpiOff = fields[4] == "" ? 0.0f : float.Parse(fields[4]);
+            BpiDef = fields[5] == "" ? 0.0f : float.Parse(fields[5]);
+            RecordW = fields[6] == "" ? 0 : int.Parse(fields[6]);
+            RecordL = fields[7] == "" ? 0 : int.Parse(fields[7]);
+            ConferenceW = fields[8] == "" ? 0 : int.Parse(fields[8]);
+            ConferenceL = fields[9] == "" ? 0 : int.Parse(fields[9]);
+            VsTop25W = fields[10] == "" ? 0 : int.Parse(fields[10]);
+            VsTop25L = fields[11] == "" ? 0 : int.Parse(fields[11]);
+            Last10W = fields[12] == "" ? 0 : int.Parse(fields[12]);
+            Last10L = fields[13] == "" ? 0 : int.Parse(fields[13]);
+            SosRank = fields[14] == "" ? 0 : int.Parse(fields[14]);
+            SorRank = fields[15] == "" ? 0 : int.Parse(fields[15]);
+            Ppg = fields[16] == "" ? 0.0f : float.Parse(fields[16]);
+            OppPgg = fields[17] == "" ? 0.0f : float.Parse(fields[17]);
+            FieldGoalPer = fields[18] == "" ? 0.0f : float.Parse(fields[18]);
+            ThreePointPer = fields[19] == "" ? 0.0f : float.Parse(fields[19]);
+            FreeThrowPer = fields[20] == "" ? 0.0f : float.Parse(fields[20]);
+        }
+    }
+
+    internal class TeamData
+    {
+        public List<Team> teams = new List<Team>();
+
+        public async Task<bool> ReadFile(Windows.Storage.StorageFile file)
+        {
+            bool toReturn = false;
+
+            IList<string> lines = await Windows.Storage.FileIO.ReadLinesAsync(file);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(',');
+                teams.Add(new Team(fields));
+            }
+
+            toReturn = true;
+            return toReturn;
+        }
+    }
+}
