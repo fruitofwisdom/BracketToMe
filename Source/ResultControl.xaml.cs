53c3399d0563c4526d7065e883b1434b4c4a114a
Ward Childress <ward.childress@gmail.com>
2022-06-29 16:27:09 -0400

    Added a ResultControl user control.
    Added a new custom user control to let me customize how to present the results without needing a TextBox, which was too complicated.

---
+++ Source/ResultControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// This custom control allows for a uniform presentation of results that look like a normal
+// TextBox, but behave much more like a fancy TextBlock. The User Control item template is
+// documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BracketToMe
+{
+	public sealed partial class ResultControl : UserControl
+	{
+		public static readonly DependencyProperty ResultProperty =
+			DependencyProperty.Register("Result", typeof(string), typeof(ResultControl),
+			new PropertyMetadata(null, new PropertyChangedCallback(OnResultChanged)));
+		public string Result
+		{
+			get { return (string)GetValue(ResultProperty); }
+			set { SetValue(ResultProperty, value); }
+		}
+
+		public ResultControl()
+		{
+			this.InitializeComponent();
+		}
+
+		private static void OnResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			ResultControl control = (ResultControl)d;
+			TextBlock text = (TextBlock)control.FindName("ResultText");
+			text.Text = control.Result;
+		}
+	}
+}
